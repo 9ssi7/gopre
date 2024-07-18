1e9ff9710c101b324d293230b018d970db00437a
9ssi7 <contact@9ssi7.dev>
2024-07-18 18:40:19 +0300

    feature/template-repo: add rescode package

---
+++ pkg/rescode/rescode.go
@@ -0,0 +1,70 @@
+package rescode
+
+// RC is a struct that contains the code, message, http status, and translateable.
+// Code is the code of the error.
+type RC struct {
+
+	// Code is the code of the error.
+	Code uint64
+
+	// Message is the message of the error.
+	Message string
+
+	// StatusCode is the http/rpc status code of the error.
+	StatusCode int
+
+	// Data is the data of the error.
+	Data any
+
+	// Error is the error of the error. (if the error is not nil)
+	errors []error
+}
+
+type RcCreator func(err ...error) *RC
+
+// New is a function to create a new RC.
+func New(code uint64, status int, message string, data ...any) RcCreator {
+	var d any
+	if len(data) > 0 {
+		d = data[0]
+	}
+	return func(err ...error) *RC {
+		return &RC{
+			Code:       code,
+			Message:    message,
+			Data:       d,
+			StatusCode: status,
+			errors:     err,
+		}
+	}
+}
+
+// JSON is a function to return the RC as a JSON.
+func (r *RC) JSON(msgs ...string) map[string]interface{} {
+	msg := r.Message
+	if len(msgs) > 0 {
+		msg = msgs[0]
+	}
+	json := map[string]interface{}{
+		"code": r.Code,
+	}
+	if r.Message != "" {
+		json["message"] = msg
+	}
+	if r.Data != nil {
+		json["data"] = r.Data
+	}
+	return json
+}
+
+func (r *RC) SetData(data any) {
+	r.Data = data
+}
+
+// Error is a function to return the message of the RC.
+func (r *RC) Error() string {
+	if len(r.errors) > 0 {
+		return r.errors[0].Error()
+	}
+	return r.Message
+}
---
+++ pkg/rescode/rescode_test.go
@@ -0,0 +1,142 @@
+package rescode
+
+import (
+	"errors"
+	"testing"
+)
+
+func TestNew(t *testing.T) {
+	tests := []struct {
+		name    string
+		code    uint64
+		status  int
+		message string
+		data    any
+		err     error
+		want    *RC
+	}{
+		{
+			name:    "Basic",
+			code:    100,
+			status:  200,
+			message: "Success",
+			want:    &RC{Code: 100, Message: "Success", StatusCode: 200},
+		},
+		{
+			name:    "WithData",
+			code:    101,
+			status:  201,
+			message: "Created",
+			data:    map[string]string{"foo": "bar"},
+			want:    &RC{Code: 101, Message: "Created", StatusCode: 201, Data: map[string]string{"foo": "bar"}},
+		},
+		{
+			name:    "WithError",
+			code:    500,
+			status:  500,
+			message: "Internal Server Error",
+			err:     errors.New("database error"),
+			want:    &RC{Code: 500, Message: "Internal Server Error", StatusCode: 500, errors: []error{errors.New("database error")}},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			creator := New(tt.code, tt.status, tt.message, tt.data)
+			rc := creator(tt.err)
+
+			if rc.Code != tt.want.Code || rc.Message != tt.want.Message || rc.StatusCode != tt.want.StatusCode {
+				t.Errorf("New() = %v, want %v", rc, tt.want)
+			}
+
+			if tt.data != nil {
+				if rc.Data == nil {
+					t.Errorf("New() data = %v, want %v", rc.Data, tt.want.Data)
+				}
+			}
+
+			if tt.err != nil {
+				if rc.Error() != tt.err.Error() {
+					t.Errorf("New() error = %v, want %v", rc.Error(), tt.err.Error())
+				}
+			}
+		})
+	}
+}
+
+func TestRC_SetData(t *testing.T) {
+	rc := &RC{}
+	data := "some data"
+	rc.SetData(data)
+
+	if rc.Data != data {
+		t.Errorf("SetData() = %v, want %v", rc.Data, data)
+	}
+}
+
+func TestRC_JSON(t *testing.T) {
+	tests := []struct {
+		name string
+		rc   *RC
+		msgs []string
+		want map[string]interface{}
+	}{
+		{
+			name: "Basic",
+			rc:   &RC{Code: 100, Message: "Success"},
+			want: map[string]interface{}{"code": uint64(100), "message": "Success"},
+		},
+		{
+			name: "Empty Message",
+			rc:   &RC{Code: 100},
+			want: map[string]interface{}{"code": uint64(100)},
+		},
+		{
+			name: "WithData",
+			rc:   &RC{Code: 100, Message: "Success", Data: "some data"},
+			want: map[string]interface{}{"code": uint64(100), "message": "Success", "data": "some data"},
+		},
+		{
+			name: "WithCustomMessage",
+			rc:   &RC{Code: 100, Message: "Success"},
+			msgs: []string{"Custom Message"},
+			want: map[string]interface{}{"code": uint64(100), "message": "Custom Message"},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := tt.rc.JSON(tt.msgs...)
+			if tt.want["code"] != got["code"] || tt.want["message"] != got["message"] || tt.want["data"] != got["data"] {
+				t.Errorf("JSON() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestRC_Error(t *testing.T) {
+	tests := []struct {
+		name    string
+		rc      *RC
+		wantErr string
+	}{
+		{
+			name:    "With Message",
+			rc:      &RC{Message: "Error message"},
+			wantErr: "Error message",
+		},
+		{
+			name:    "With Errors",
+			rc:      &RC{Message: "Error message", errors: []error{errors.New("underlying error")}},
+			wantErr: "underlying error",
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if err := tt.rc.Error(); err != tt.wantErr {
+				t.Errorf("Error() = %v, want %v", err, tt.wantErr)
+			}
+		})
+	}
+}
